ca050646ec80255ede369e6f95420bf353f7ef35
Connor Atherton <c.liam.atherton@gmail.com>
2015-05-04 20:12:30 -0700

    Add double dash regex match

--- lib/minimist.rb
+++ lib/minimist.rb
@@ -7,7 +7,7 @@ module Minimist
   class << self
     REGEX  = {
       command: /^[A-Za-z]/,
-      double_dash: /^--.+=/,
+      double_dash: /^--.+/,
       double_dash_negation: /^--no-.+/,
       single_dash: /^-[^-]+/
     }
@@ -17,7 +17,7 @@ module Minimist
     # with actions and options passed
     #
     def parse(argv)
-      @@argv = argv
+      @argv = argv
 
       out = {
         commands: [],
@@ -27,19 +27,13 @@ module Minimist
       type = nil
 
       argv.each_with_index do |arg, i|
-        binding.pry
+        # reset
+        type = nil
 
         if skip
           skip = false
           next
         end
-
-        # next if skip
-
-        # reset
-        type = nil
-
-        (skip = false && next) if skip
 
         REGEX.each do |name, regex|
           match = arg =~ regex
@@ -54,7 +48,6 @@ module Minimist
         # arg type method should indicate whether it wants
         # to skip the next value or not
         #
-        binding.pry
         out, skip = send(type, arg, i, out)
 
         puts "#{i} #{arg} #{type} - should skip #{skip}"
@@ -65,7 +58,7 @@ module Minimist
 
     private
 
-    def command(arg, index, argv_object)
+    def command(arg, _, argv_object)
       argv_object[:commands] << arg
       [argv_object, false]
     end
@@ -75,20 +68,19 @@ module Minimist
     #
     def single_dash(arg, index, argv_object)
       #
-      # match -abc arg types
+      # match -abc, -n5 arg types
       #
-      # TODO test for -n5 case
-      if arg =~ /^[A-Za-z][0-9]$/
-        # test it
-      elsif @@argv[index + 1] =~ /^(\d|[A-Za-z])/
-        argv_object[:options][arg.slice(1..-1).to_sym] = try_convert(@@argv[index + 1])
+      if match_data = arg.match(/^-([A-Za-z])([0-9])$/)
+        argv_object[:options][match_data[1].to_sym] = match_data[2]
+      elsif @argv[index + 1] =~ /^(\d|[A-Za-z])/
+        argv_object[:options][arg.slice(1..-1).to_sym] = try_convert(@argv[index + 1])
         should_skip = true
       else
         #
         # loop through and apply each letter to the
         # options object
         #
-        arg.slice(1..-1).split("").each do |letter|
+        arg.slice(1..-1).split('').each do |letter|
           argv_object[:options][letter.to_sym] = true
         end
       end
@@ -100,6 +92,14 @@ module Minimist
       #
       # match --skip=true arg types
       #
+      # TODO need to match --plan-changed=true types
+      match_data = arg.match(/^--([A-Za-z]*)=?([A-Za-z0-9]*)/)
+
+      if !match_data[2].empty?
+        argv_object[:options][match_data[1].to_sym] = try_convert(match_data[2])
+      else
+        argv_object[:options][match_data[1].to_sym] = true
+      end
 
       [argv_object, false]
     end
